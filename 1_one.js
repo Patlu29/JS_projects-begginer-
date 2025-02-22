405ca902341fcc7a23918ab2cd6e4c86995a9094
PRAKASH RAJAN <prakashrajan2908@gmail.com>
2025-02-22 15:29:27 +0530

    with conditions start a game

--- 1_one.js
+++ 1_one.js
@@ -2,4 +2,26 @@ const prompt = require("prompt-sync")()
 
 const name = prompt("tell me your name: ")
 
-console.log(name)
+console.log(`i think your name is ${name.toUpperCase()} right? Let's get into the game..`)
+
+const shoulWePlay = function() { 
+    decision = prompt("Do you want to play (Yes/No): ").toLowerCase()
+    return decision
+}
+
+var choice ;
+
+while(true) {
+
+    choice = shoulWePlay()
+
+    if (choice === 'yes') {
+    console.log("Really....Let's start the game...!")
+    break
+}else if (choice === 'no') {
+    console.log("Ok See you later...")
+    break
+}else {
+    console.log("Tell me are you ready to play or not?")
+}
+}
