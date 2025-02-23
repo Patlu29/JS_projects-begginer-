263ea034db3d6ca13c3cb1b23a1aaa2e53172e8a
PRAKASH RAJAN <prakashrajan2908@gmail.com>
2025-02-23 13:05:37 +0530

    prettier

--- 1_one.js
+++ 1_one.js
@@ -1,27 +1,28 @@
-const prompt = require("prompt-sync")()
+const prompt = require("prompt-sync")();
 
-const name = prompt("tell me your name: ")
+const name = prompt("tell me your name: ");
 
-console.log(`i think your name is ${name.toUpperCase()} right? Let's get into the game..`)
+console.log(
+  `i think your name is ${name.toUpperCase()} right? Let's get into the game..`
+);
 
-const shoulWePlay = function() { 
-    decision = prompt("Do you want to play (Yes/No): ").toLowerCase()
-    return decision
-}
+const shoulWePlay = function () {
+  decision = prompt("Do you want to play (Yes/No): ").toLowerCase();
+  return decision;
+};
 
-var choice ;
+var choice;
 
-while(true) {
+while (true) {
+  choice = shoulWePlay();
 
-    choice = shoulWePlay()
-
-    if (choice === 'yes') {
-    console.log("Really....Let's start the game...!")
-    break
-}else if (choice === 'no') {
-    console.log("Ok See you later...")
-    break
-}else {
-    console.log("Tell me are you ready to play or not?")
-}
+  if (choice === "yes") {
+    console.log("Really....Let's start the game...!");
+    break;
+  } else if (choice === "no") {
+    console.log("Ok See you later...");
+    break;
+  } else {
+    console.log("Tell me are you ready to play or not?");
+  }
 }
