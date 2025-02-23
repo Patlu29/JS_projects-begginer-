6273f6e6ef38778d51d28dbf5614045cf0df68bc
PRAKASH RAJAN <prakashrajan2908@gmail.com>
2025-02-23 14:04:03 +0530

    first shop coded

--- 1_one.js
+++ 1_one.js
@@ -17,8 +17,33 @@ while (true) {
   choice = shoulWePlay();
 
   if (choice === "yes") {
-    console.log("Really....Let's start the game...!");
-    break;
+    console.log("Really....Let's start the game...!\n");
+    console.log("Now you're entered in a market which store you want explore...Select below")
+    while (true) {
+    const shopList = ['Vegetable shop', 'Jwellary shop', 'Super market', 'Shoe mart']
+    console.table(shopList)
+    const shopNo = parseInt(prompt("Which shop you want to go..? Enter(0-3) or Exit(any number): "))
+    if (shopNo < 0 || shopNo > 3) {
+        break
+    }else if (shopNo === 0) {
+        vegList = ["Tomato", "Potato", "Cauliflower", "Cucumber", "Onion", "Meat", "Milk"]
+        console.log("Today list.. Please select from below...")
+        while (true) {
+        console.table(vegList)
+        const selectedVeg = parseInt(prompt("Which one you want..Enter(0-6) or Exit(any number): "))
+        if (selectedVeg === 6) {
+            console.log(`\nthanks for visiting our shop...here's your ${vegList[selectedVeg].toUpperCase()} and your bill is $10...`)
+            break
+        }else if (selectedVeg >= 0 && selectedVeg <= 5) {
+            console.log(`\nthanks for visiting our shop...here's your ${vegList[selectedVeg].toUpperCase()} and your bill is $25...`)
+            break
+        }else {
+            console.log("\nThank you for visiting..")
+            break
+        }
+    }
+    }
+    }
   } else if (choice === "no") {
     console.log("Ok See you later...");
     break;
